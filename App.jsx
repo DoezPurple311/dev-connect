b174da6f12a3ee7160c7cd4d7c6f5caa66e10383
Nguyen Nhu Quy <snedegerblkettbl914@hotmail.com>
2025-04-09 11:14:20 +0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Messages from './pages/Messages';
+import Navbar from './components/Navbar';
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/messages" element={<Messages />} />
+      </Routes>
+    </Router>
+  );
+}
